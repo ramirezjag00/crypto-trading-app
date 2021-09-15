eefb9b81bbe678d5b16f12eb8b272ca4c2a36456
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-16 03:33:29 +0800

    Check and fix eslint errors

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import React from 'react'
+import { SafeAreaView, StyleSheet, Text } from 'react-native'
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <SafeAreaView>
       <Text style={styles.title}>Hello World!</Text>
     </SafeAreaView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     textAlign: 'center',
   },
-});
+})
 
-export default App;
+export default App
