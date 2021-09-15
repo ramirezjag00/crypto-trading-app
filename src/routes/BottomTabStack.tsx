53b0fe2b61cc70ca330a7fcb1e55a81b048d423b
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-16 06:29:53 +0800

    Add minimal BottomTabStack

---
+++ src/routes/BottomTabStack.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+import MarketStack from './MarketStack'
+import BottomTabParamList from '../types/navigation/bottomTab'
+
+const BottomTabBar = createBottomTabNavigator<BottomTabParamList>()
+
+const BottomTabStack: React.FC = () => {
+  return (
+    <SafeAreaView style={styles.safeAreaView}>
+      <BottomTabBar.Navigator
+        initialRouteName="Market"
+        screenOptions={{ headerShown: false }}>
+        <BottomTabBar.Screen name="Market" component={MarketStack} />
+      </BottomTabBar.Navigator>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
+})
+
+export default BottomTabStack
