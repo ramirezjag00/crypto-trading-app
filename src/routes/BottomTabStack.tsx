2bf2f7af0079bc0742da3ca6a9c2a05496405b82
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-17 01:31:41 +0800

    Create screenOptions handler

--- src/routes/BottomTabStack.tsx
+++ src/routes/BottomTabStack.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet } from 'react-native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { ImageSourcePropType, SafeAreaView, StyleSheet } from 'react-native'
+import {
+  BottomTabNavigationOptions,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs'
+import { RouteProp } from '@react-navigation/core'
 
 import MarketStack from '@routes/MarketStack'
 import type BottomTabParamList from '@customtypes/navigation/bottomTab'
@@ -10,6 +14,33 @@ import coinsIcon from '@assets/coins.png'
 const BottomTabBar = createBottomTabNavigator<BottomTabParamList>()
 
 const BottomTabStack: React.FC = () => {
+  const { colors } = theme
+
+  const handleScreenOptions = ({
+    route,
+  }: {
+    route: RouteProp<BottomTabParamList, 'Market'>
+  }): BottomTabNavigationOptions => ({
+    tabBarIcon: ({ color }): React.ReactNode => {
+      let icon: ImageSourcePropType = 0
+      const iconStyles = StyleSheet.flatten([
+        styles.tabIcon,
+        { tintColor: color },
+      ])
+
+      if (route.name === 'Market') {
+        icon = coinsIcon as ImageSourcePropType
+      }
+
+      return <Image source={icon} style={iconStyles} />
+    },
+    headerShown: false,
+    tabBarActiveTintColor: colors?.primary,
+    tabBarInactiveTintColor: colors?.light,
+    tabBarLabelStyle: styles.tabLabel,
+    tabBarStyle: styles.tabBar,
+  })
+
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <BottomTabBar.Navigator
