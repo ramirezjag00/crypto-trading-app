b7ce54013939788e0d58efefa4230a6199d86179
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-18 05:48:40 +0800

    Add coins list header title

--- src/components/screens/market/CoinsScreen.tsx
+++ src/components/screens/market/CoinsScreen.tsx
@@ -1,8 +1,20 @@
 import React, { useEffect, useState } from 'react'
-import { SafeAreaView, StyleSheet, Text } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 
 import { CoinDefault } from '@customtypes/coins/coin'
 import { get } from '@api'
+import theme from '@theme'
+
+const renderItem = ({ item }: { item: CoinDefault }) => (
+  <View key={item?.id} style={styles.coinContainer}>
+    <Text style={styles.coinDetail} numberOfLines={2}>
+      {item?.name}
+    </Text>
+    <Text style={styles.coinDetail} numberOfLines={2}>
+      {item?.symbol}
+    </Text>
+  </View>
+)
 
 const CoinsScreen: React.FC = () => {
   const [coins, setCoins] = useState<CoinDefault[]>([])
@@ -28,6 +40,9 @@ const CoinsScreen: React.FC = () => {
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <Text style={styles.title}>List of cryptocurrencies here</Text>
+      <View style={styles.coinsContainer}>
+        {renderItem({ item: { id: 'ID', name: 'Name', symbol: 'SYMBOL' } })}
+      </View>
     </SafeAreaView>
   )
 }
@@ -35,10 +50,25 @@ const CoinsScreen: React.FC = () => {
 const styles = StyleSheet.create({
   safeAreaView: {
     flex: 1,
+    backgroundColor: theme?.colors?.dark,
   },
   title: {
+    color: theme?.colors?.white,
     fontSize: 24,
     textAlign: 'center',
+  },
+  coinsContainer: {
+    paddingHorizontal: 20,
+  },
+  coinContainer: {
+    flexDirection: 'row',
+  },
+  coinDetail: {
+    flex: 2,
+    textAlign: 'center',
+    fontSize: 14,
+    color: theme?.colors?.white,
+    paddingVertical: 5,
   },
 })
 
