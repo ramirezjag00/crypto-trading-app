c8c9de4670871812706c4cfafdd98cb754c0199b
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-18 05:47:03 +0800

    Add initial call for coins list in coin gecko api

--- src/components/screens/market/CoinsScreen.tsx
+++ src/components/screens/market/CoinsScreen.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView, StyleSheet, Text } from 'react-native'
 
+import { CoinDefault } from '@customtypes/coins/coin'
+import { get } from '@api'
+
 const CoinsScreen: React.FC = () => {
+  const [coins, setCoins] = useState<CoinDefault[]>([])
+  const [hasError, setHasError] = useState<boolean>(false)
+
+  const getCoins = async (): Promise<void> => {
+    try {
+      const response = await get<CoinDefault[]>('/coins/list', {
+        include_platform: false,
+      })
+      setCoins(response)
+    } catch (error) {
+      setHasError(true)
+    }
+  }
+
+  useEffect(() => {
+    if (!coins?.length) {
+      getCoins()
+    }
+  }, [coins])
+
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <Text style={styles.title}>List of cryptocurrencies here</Text>
