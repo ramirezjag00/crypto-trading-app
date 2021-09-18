9c0c0667db3d84a8b7411ef79fa8659e6de6811a
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-18 09:21:31 +0800

    Create coinsApi with RTK Query

--- src/redux/coins/coinsSlice.ts
+++ src/redux/coins/coinsSlice.ts
@@ -1,1 +1,20 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+import { CoinDefault } from '@customtypes/coins/coin'
+import { BASE_URL } from '@constants/config'
+
+const coinsApi = createApi({
+  reducerPath: 'coinsApi',
+  tagTypes: ['Coins'],
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  endpoints: (builder) => ({
+    fetchCoins: builder.query<string, void>({
+      query: () => 'coins/list/?include_platform=false',
+      transformResponse: (response: CoinDefault[]) => {
+        let ids = ''
+        response.forEach((coin) => (ids += `${coin?.id},`))
+        return ids
+      },
+    }),
+  }),
+})
