5054aad53e4ed8b31307cc4f76463c51f595d35b
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-19 02:45:53 +0800

    Add trading candle and dark shade theme colors

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -2,7 +2,11 @@ const colors = {
   primary: '#8BC540',
   dark: '#222222',
   darkShadeLight20: '#4e4e4e',
+  darkShadeLight40: '#7a7a7a',
+  darkShadeLight90: '#e9e9e9',
   white: '#FFFFFF',
+  bearishRed: '#F6455D',
+  bullishGreen: '#10CB81',
 }
 
 const theme = {
