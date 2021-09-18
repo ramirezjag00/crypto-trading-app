f15275df94db269e27146d65acaac4b679e86373
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-18 10:09:42 +0800

    Add coinUnitsApi reducer in store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import { coinsApi } from './api/coinsApi'
+import { coinUnitsApi } from './api/coinUnitsApi'
 
 const store = configureStore({
   reducer: {
     [coinsApi?.reducerPath]: coinsApi?.reducer,
+    [coinUnitsApi?.reducerPath]: coinUnitsApi?.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(coinsApi?.middleware),
+    getDefaultMiddleware().concat(
+      coinsApi?.middleware,
+      coinUnitsApi?.middleware,
+    ),
 })
 
 export default store
