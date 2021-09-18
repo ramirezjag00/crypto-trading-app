7a174af78b983dd7f85416932dd9fb8c9aad7379
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-18 10:08:14 +0800

    Create coinUnitsApi

--- src/redux/api/coinUnitsApi.ts
+++ src/redux/api/coinUnitsApi.ts
@@ -1,1 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+import { BASE_URL } from '@constants/config'
+import { CoinUnits } from '@customtypes/coins/coin'
+
+const coinUnitsApi = createApi({
+  reducerPath: 'coinUnitsApi',
+  tagTypes: ['CoinUnits'],
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  endpoints: (builder) => ({
+    fetchCoinUnits: builder.query<CoinUnits, void>({
+      query: () => 'simple/supported_vs_currencies',
+    }),
+  }),
+})
