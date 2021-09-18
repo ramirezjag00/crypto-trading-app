ce42b4224bba222ad1d2bc0b46c8ec189a9099be
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-19 03:50:09 +0800

    Create CoinsList and implement useFetchCoinDetailsQuery

---
+++ src/components/screens/market/components/CoinsList.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react'
+import {
+  CoinDefaultChunkType,
+} from '@customtypes/coins/coin'
+import { useFetchCoinDetailsQuery } from '@store/api/coinDetails'
+import { POLLING_INTERVAL } from '@constants/config'
+
+const CoinsList: React.FC<Props> = (props) => {
+  const {
+    activeUnit,
+    coinIds = [],
+    isFetchingCoinIds,
+    activeCoinIdsIndex,
+  } = props
+  const {
+    data: coinDetails,
+    refetch,
+    // error,
+  } = useFetchCoinDetailsQuery(
+    {
+      ids:
+        coinIds?.[activeCoinIdsIndex]?.map((coin) => coin?.id).join(',') || '',
+      unit: activeUnit,
+    },
+    {
+      skip: isFetchingCoinIds && !coinIds?.length,
+      pollingInterval: POLLING_INTERVAL,
+      refetchOnFocus: true,
+    },
+  )
+  console.log('coinDetails', coinDetails)
+  // console.log('error', error)
+
+  useEffect(() => {
+    refetch()
+  }, [refetch])
+
+  return (
+  )
+}
+
+export default CoinsList
