3deb490410eeafeb313758aad222518aaf026b55
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-19 02:47:00 +0800

    Define constant for max requests of free CG api tier

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,3 +1,7 @@
 const BASE_URL = 'https://api.coingecko.com/api/v3/'
 
-export { BASE_URL }
+const MAX_API_CALL = 50
+const MINUTE_TO_MS = 60000
+const POLLING_INTERVAL = MINUTE_TO_MS / MAX_API_CALL
+
+export { BASE_URL, POLLING_INTERVAL }
