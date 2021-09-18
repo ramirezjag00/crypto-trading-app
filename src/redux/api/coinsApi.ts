c2ba521830973702abfc84162ec5cee6cdc36d98
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-19 02:43:49 +0800

    Reformat coinsApi response into chunk subArrays

--- src/redux/api/coinsApi.ts
+++ src/redux/api/coinsApi.ts
@@ -1,21 +1,31 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-import { CoinDefaultType } from '@customtypes/coins/coin'
+import {
+  CoinDefaultResponseType,
+  CoinDefaultChunkType,
+} from '@customtypes/coins/coin'
 import { BASE_URL } from '@constants/config'
+
+const CHUNK = 30
 
 const coinsApi = createApi({
   reducerPath: 'coinsApi',
   tagTypes: ['Coins'],
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: (builder) => ({
-    fetchCoins: builder.query<string, void>({
-      query: () => 'coins/list/?include_platform=false',
-      transformResponse: (response: CoinDefaultType[]) => {
-        let ids = ''
-        response.forEach((coin) => {
-          ids = `${ids}${coin?.id},`
-        })
-        return ids
+    fetchCoins: builder.query<CoinDefaultChunkType, void>({
+      query: () => 'coins/list?include_platform=false',
+      transformResponse: (response: CoinDefaultResponseType[]) => {
+        let i: number
+        let j: number
+        let temporary: CoinDefaultResponseType[] = []
+        const result: CoinDefaultChunkType = []
+
+        for (i = 0, j = response?.length; i < j; i += CHUNK) {
+          temporary = response?.slice(i, i + CHUNK)
+          result?.push(temporary)
+        }
+        return result
       },
     }),
   }),
