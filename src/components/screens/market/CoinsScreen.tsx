72f51aa530606180b690278ae1b27850456f2611
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-18 20:23:45 +0800

    Delete title

--- src/components/screens/market/CoinsScreen.tsx
+++ src/components/screens/market/CoinsScreen.tsx
@@ -13,7 +13,6 @@ const CoinsScreen: React.FC = () => {
   console.log('coinIds', coinIds)
   return (
     <SafeAreaView style={styles.safeAreaView}>
-      <Text style={styles.title}>List of cryptocurrencies here</Text>
       <CoinFilters
         activeUnit={activeUnit}
         setActiveUnit={setActiveUnit}
@@ -27,11 +26,6 @@ const styles = StyleSheet.create({
   safeAreaView: {
     flex: 1,
     backgroundColor: theme?.colors?.dark,
-  },
-  title: {
-    color: theme?.colors?.white,
-    fontSize: 24,
-    textAlign: 'center',
   },
 })
 
