ebeb67ef7801be9c03d23d7c2d2a74a25218b8aa
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-19 02:42:55 +0800

    Create coinDetailsApi using createApi RTK Query

---
+++ src/redux/api/coinDetails.ts
@@ -0,0 +1,32 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+import { BASE_URL } from '@constants/config'
+import { CoinDetailsType } from '@customtypes/coins/coin'
+
+const coinDetailsApi = createApi({
+  reducerPath: 'coinDetailsApi',
+  tagTypes: ['CoinDetails'],
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  endpoints: (builder) => ({
+    fetchCoinDetails: builder.query<
+      CoinDetailsType,
+      { ids: string; unit: string }
+    >({
+      query: ({ ids, unit }) => {
+        return {
+          url: 'simple/price',
+          params: {
+            ids,
+            vs_currencies: unit,
+            include_24hr_vol: true,
+            include_24hr_change: true,
+          },
+        }
+      },
+    }),
+  }),
+})
+
+const { useFetchCoinDetailsQuery } = coinDetailsApi
+
+export { coinDetailsApi, useFetchCoinDetailsQuery }
