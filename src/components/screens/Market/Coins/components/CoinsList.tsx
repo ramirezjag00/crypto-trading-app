a9ed0f714481776d6889a2a03f7a6111268638ec
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-19 20:18:02 +0800

    Decouple coin list item component from coin list

--- src/components/screens/Market/Coins/components/CoinsList.tsx
+++ src/components/screens/Market/Coins/components/CoinsList.tsx
@@ -1,26 +1,17 @@
 import React, { Fragment, useCallback, useEffect, useState } from 'react'
-import {
-  Dimensions,
-  FlatList,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native'
+import { FlatList, StyleSheet, Text } from 'react-native'
 import { useIsFocused } from '@react-navigation/core'
 
 import {
   CoinDefaultChunkType,
   CoinDefaultResponseType,
 } from '@customtypes/coins/coin'
-import { metricSuffix } from '@utils/metricSuffix'
 import theme from '@constants/theme'
 import { useFetchCoinDetailsQuery } from '@store/api/coinDetails'
 import { POLLING_INTERVAL } from '@constants/config'
 import { coinListTitles } from '@constants/coins'
 import LoadingCoins from '@common/LoadingCoins'
-
-const SCREEN_WIDTH = Dimensions.get('screen')?.width
+import CoinListItem from './CoinListItem'
 
 interface Props {
   activeUnit: string
@@ -82,49 +73,19 @@ const CoinsList: React.FC<Props> = (props) => {
     setCoins,
   ])
 
-  const onPress = (item: CoinDefaultResponseType) => () =>
-    console.log('item', item)
-
-  const renderCoinDetails = ({ item }: { item: CoinDefaultResponseType }) => {
-    const coin24hVol = metricSuffix(
-      coinDetailsData?.[item?.id]?.[`${activeUnit}_24h_vol`] || 0,
-    )
-    const coinPrice =
-      coinDetailsData?.[item?.id]?.[activeUnit] || (0).toFixed(2)
-    const coin24hChg = (
-      coinDetailsData?.[item?.id]?.[`${activeUnit}_24h_change`] || 0
-    )?.toFixed(2)
-    const change24HStyles = StyleSheet.flatten([
-      styles.coin24HChgCointainer,
-      coin24hChg?.includes('-')
-        ? styles.coin24HChgBearish
-        : coin24hChg === '0.00'
-        ? {}
-        : styles.coin24HChgBullish,
-    ])
-
-    return (
-      <TouchableOpacity style={styles.coinDetailsItem} onPress={onPress(item)}>
-        <View>
-          <Text style={styles.coinMain} numberOfLines={3}>
-            {item?.symbol}
-            <Text style={styles.coinMeta}>/{activeUnit}</Text>
-          </Text>
-          <Text style={styles.coinMeta}>
-            Vol. {coin24hVol} {activeUnit}
-          </Text>
-        </View>
-        <Text style={styles.coinPrice}>{coinPrice}</Text>
-        <View style={change24HStyles}>
-          <Text style={styles.coin24hChg}>{coin24hChg}%</Text>
-        </View>
-      </TouchableOpacity>
-    )
-  }
-
   const renderCoinTitle = ({ item }: { item: string }) => (
     <Text style={styles.coinListTitle}>{item}</Text>
   )
+
+  const renderCoinDetails = ({ item }: { item: CoinDefaultResponseType }) => {
+    return (
+      <CoinListItem
+        coinDetailsData={coinDetailsData}
+        activeUnit={activeUnit}
+        coin={item}
+      />
+    )
+  }
 
   if (!data?.length) {
     return <LoadingCoins />
@@ -174,47 +135,6 @@ const styles = StyleSheet.create({
   },
   coinDetailsContentContainer: {
     marginHorizontal: 20,
-  },
-  coinDetailsItem: {
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flexDirection: 'row',
-    paddingVertical: 10,
-  },
-  coinMain: {
-    width: SCREEN_WIDTH * 0.3,
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: theme?.colors?.white,
-    textTransform: 'uppercase',
-  },
-  coinPrice: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: theme?.colors?.white,
-    textTransform: 'uppercase',
-  },
-  coin24HChgCointainer: {
-    backgroundColor: theme?.colors?.darkShadeLight40,
-    padding: 10,
-    borderRadius: 4,
-  },
-  coin24HChgBearish: {
-    backgroundColor: theme?.colors?.bearishRed,
-  },
-  coin24HChgBullish: {
-    backgroundColor: theme?.colors?.bullishGreen,
-  },
-  coin24hChg: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: theme?.colors?.white,
-    textTransform: 'uppercase',
-  },
-  coinMeta: {
-    fontWeight: 'normal',
-    color: theme.colors?.darkShadeLight90,
-    fontSize: 12,
   },
   coinListTitle: {
     fontSize: 12,
