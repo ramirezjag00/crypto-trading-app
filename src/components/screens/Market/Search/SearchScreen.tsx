3ec3964b08d22003805c56dfae32da253cce4cb6
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-19 21:38:10 +0800

    Change SearchScreen BGColor

--- src/components/screens/Market/Search/SearchScreen.tsx
+++ src/components/screens/Market/Search/SearchScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text } from 'react-native'
+import { SafeAreaView, StyleSheet } from 'react-native'
 import { useRoute } from '@react-navigation/core'
 
 import theme from '@constants/theme'
@@ -8,21 +8,14 @@ import { MarketStackRouteProp } from '@customtypes/navigation/market'
 const SearchScreen: React.FC = () => {
   const route = useRoute<MarketStackRouteProp<'SearchScreen'>>()
   console.log('coinListData', route?.params?.coinListData)
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Search Screen</Text>
-    </SafeAreaView>
-  )
+
+  return <SafeAreaView style={styles.safeAreaView} />
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeAreaView: {
     flex: 1,
-  },
-  title: {
-    fontWeight: 'bold',
-    color: theme?.colors?.dark,
-    textAlign: 'center',
+    backgroundColor: theme?.colors?.dark,
   },
 })
 
