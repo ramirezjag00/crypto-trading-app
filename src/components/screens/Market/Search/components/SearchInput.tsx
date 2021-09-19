482ddc8462ac6e1afc45feaaeb66dd576bcf201a
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-19 21:43:28 +0800

    Add clear text input and cancel to go back features

--- src/components/screens/Market/Search/components/SearchInput.tsx
+++ src/components/screens/Market/Search/components/SearchInput.tsx
@@ -1,8 +1,17 @@
 import React, { useRef, useState } from 'react'
-import { Platform, StyleSheet, TextInput, View } from 'react-native'
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import { useNavigation } from '@react-navigation/core'
 import debounce from 'lodash.debounce'
 
 import theme from '@constants/theme'
+import { MarketStackNavigationProp } from '@customtypes/navigation/market'
 import { sanitizeString } from '@utils/string'
 
 interface Props {
@@ -12,6 +21,7 @@ interface Props {
 
 const SearchInput: React.FC<Props> = (props) => {
   const { value, setValue } = props
+  const navigation = useNavigation<MarketStackNavigationProp<'SearchScreen'>>()
 
   const inputRef = useRef<TextInput>(null)
   const inputStyles = StyleSheet.flatten([
@@ -28,9 +38,13 @@ const SearchInput: React.FC<Props> = (props) => {
     }
   }
 
+  const onClearText = (): void => setValue('')
+
   const [debounceSearchText] = useState(() => debounce(onTextChange, 300))
 
   const handleSearchTextChange = (text: string) => debounceSearchText(text)
+
+  const onPressCancel = () => navigation.goBack()
 
   return (
     <View style={styles.searchContainer}>
@@ -48,7 +62,15 @@ const SearchInput: React.FC<Props> = (props) => {
           placeholderTextColor={theme?.colors?.darkShadeLight60}
           defaultValue={value}
         />
+        {!!value && (
+          <TouchableOpacity style={styles.clearContainer} onPress={onClearText}>
+            <Text style={styles.clear}>✕</Text>
+          </TouchableOpacity>
+        )}
       </View>
+      <Text style={styles.cancel} onPress={onPressCancel}>
+        Cancel
+      </Text>
     </View>
   )
 }
@@ -87,6 +109,25 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 12,
+  },
+  clearContainer: {
+    height: 12,
+    width: 12,
+    borderColor: theme?.colors?.darkShadeLight60,
+    borderWidth: 1,
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  clear: {
+    fontSize: 10,
+    color: theme?.colors?.darkShadeLight60,
+  },
+  cancel: {
+    color: theme?.colors?.primary,
+    fontSize: 11,
+    fontWeight: 'bold',
+    alignSelf: 'center',
   },
 })
 
