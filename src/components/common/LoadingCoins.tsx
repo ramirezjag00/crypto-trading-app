d4c6ef24a08e9062de0d0edef4c7df38c8b93141
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-19 19:27:36 +0800

    Remove view wrapper of LoadingCoins component

--- src/components/common/LoadingCoins.tsx
+++ src/components/common/LoadingCoins.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { ActivityIndicator, StyleSheet, View } from 'react-native'
+import { ActivityIndicator, StyleSheet } from 'react-native'
 
 import theme from '@constants/theme'
 
 const LoadingCoins: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <ActivityIndicator color={theme?.colors?.primary} size="large" />
-    </View>
+    <ActivityIndicator
+      color={theme?.colors?.primary}
+      size="large"
+      style={styles.container}
+    />
   )
 }
 
