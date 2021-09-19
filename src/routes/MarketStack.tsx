cd8007de599e0f11317723e3ec58c42ff47bf243
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-19 17:38:08 +0800

    Organize state colocation folder structure

--- src/routes/MarketStack.tsx
+++ src/routes/MarketStack.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import CoinsScreen from '@screens/market/CoinsScreen'
+import CoinsScreen from '@screens/Market/Coins/CoinsScreen'
 import type MarketStackParamList from '@customtypes/navigation/market'
 
 const MarketStack = createNativeStackNavigator<MarketStackParamList>()
