00fe0c27d25ccd16d01e998ec80e2e6cb6dc784c
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-21 01:49:18 +0800

    Add other coin to add details and styles

--- src/components/common/AddToTradeModal.tsx
+++ src/components/common/AddToTradeModal.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text } from 'react-native'
 
 import theme from '@constants/theme'
 import Modal from './Modal'
+import QuantityController from './QuantityController'
 import { CoinAddTradeModalType } from '@customtypes/coins/coin'
 
 interface Props {
@@ -25,6 +26,18 @@ const AddToTradeModal: React.FC<Props> = (props) => {
   return (
     <Modal isVisible={isModalVisible} onCloseModal={onCloseModal} hasContainer>
       <Text style={styles.coinName}>{activeCoin?.name}</Text>
+      <Text style={styles.coinPair}>
+        {activeCoin?.symbol}/{activeCoin?.unit}
+      </Text>
+      <Text style={styles.coinPrice}>69.69696969</Text>
+      <QuantityController
+        quantity={activeCoinQuantity}
+        onPress={onAmountChange}
+      />
+      <Text style={styles.coinTotal}>Total {activeCoin?.symbol}: </Text>
+      <Text style={styles.coinTotalPrice} numberOfLines={2}>
+        {69.69696969 * activeCoinQuantity}
+      </Text>
     </Modal>
   )
 }
@@ -36,6 +49,34 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: theme?.colors?.white,
     paddingBottom: 15,
+  },
+  coinPair: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: theme?.colors?.white,
+    textTransform: 'uppercase',
+    paddingBottom: 10,
+  },
+  coinPrice: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: theme?.colors?.white,
+    paddingBottom: 10,
+  },
+  coinTotal: {
+    paddingTop: 15,
+    fontSize: 14,
+    textAlign: 'center',
+    color: theme?.colors?.white,
+    paddingBottom: 10,
+  },
+  coinTotalPrice: {
+    width: '80%',
+    fontWeight: 'bold',
+    fontSize: 18,
+    textAlign: 'center',
+    color: theme?.colors?.primary,
+    textTransform: 'uppercase',
   },
 })
 
