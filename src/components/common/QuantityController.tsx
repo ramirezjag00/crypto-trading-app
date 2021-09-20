3d17cdc5ec0319f910007043866e2a4ca3cb28c7
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-21 04:26:50 +0800

    Add style props for Quantity Controller and do an style obj assign

--- src/components/common/QuantityController.tsx
+++ src/components/common/QuantityController.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import {
+  StyleProp,
+  StyleSheet,
+  Text,
+  TextStyle,
+  View,
+  ViewStyle,
+} from 'react-native'
 
 import theme from '@constants/theme'
 import Button from './Button'
@@ -7,32 +14,56 @@ import Button from './Button'
 interface Props {
   onPress: (isIncreasing: boolean) => () => void
   quantity: number
+  isQuantityVisible?: boolean
+  controllerStyle?: StyleProp<ViewStyle>
+  containerStyle?: StyleProp<ViewStyle>
+  textStyle?: StyleProp<TextStyle>
 }
 
 const QuantityController: React.FC<Props> = (props) => {
-  const { onPress, quantity } = props
+  const {
+    onPress,
+    quantity,
+    isQuantityVisible = true,
+    containerStyle,
+    controllerStyle,
+    textStyle,
+  } = props
+  const containerStyles = StyleSheet.flatten([
+    styles.container,
+    !isQuantityVisible && containerStyle,
+  ])
+  const quantityStyles = StyleSheet.flatten([
+    styles.quantityController,
+    !isQuantityVisible && controllerStyle,
+  ])
+  const textStyles = StyleSheet.flatten([
+    styles.controllerLabel,
+    !isQuantityVisible && textStyle,
+  ])
+
   return (
-    <View style={styles.controller}>
+    <View style={containerStyles}>
       <Button
-        buttonStyles={styles.quantityController}
+        buttonStyles={quantityStyles}
         label="-"
         onPress={onPress(false)}
-        textStyles={styles.controllerLabel}
+        textStyles={textStyles}
         disabled={!quantity}
       />
-      <Text style={styles.quantity}>{quantity}</Text>
+      {isQuantityVisible && <Text style={styles.quantity}>{quantity}</Text>}
       <Button
-        buttonStyles={styles.quantityController}
+        buttonStyles={quantityStyles}
         label="+"
         onPress={onPress(true)}
-        textStyles={styles.controllerLabel}
+        textStyles={textStyles}
       />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  controller: {
+  container: {
     width: '30%',
     flexDirection: 'row',
     justifyContent: 'space-around',
