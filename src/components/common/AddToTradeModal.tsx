e2ae2c228b05177c2fd3015d1145a86f5ec9794a
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-22 05:29:06 +0800

    Add keyboard dismiss on close addtotrade modal

--- src/components/common/AddToTradeModal.tsx
+++ src/components/common/AddToTradeModal.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Keyboard } from 'react-native'
 
 import Modal from './Modal'
 import { CoinAddTradeModalType, CoinTradeType } from '@customtypes/coins/coin'
@@ -16,13 +17,21 @@ const AddToTradeModal: React.FC<Props> = (props) => {
   const { activeCoin, isModalVisible, onCloseModal } = props
   const dispatch = useAppDispatch()
 
-  const onPressAddToTrades = (coinTrade: CoinTradeType): void => {
-    dispatch(upsertCoinTrade(coinTrade))
+  const handleCloseModal = (): void => {
+    Keyboard.dismiss()
     onCloseModal()
   }
 
+  const onPressAddToTrades = (coinTrade: CoinTradeType): void => {
+    dispatch(upsertCoinTrade(coinTrade))
+    handleCloseModal()
+  }
+
   return (
-    <Modal isVisible={isModalVisible} onCloseModal={onCloseModal} hasContainer>
+    <Modal
+      isVisible={isModalVisible}
+      onCloseModal={handleCloseModal}
+      hasContainer>
       <TradeCard
         buttonLabel="Add to Trades"
         onPressButton={onPressAddToTrades}
