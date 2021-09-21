41d4f627093c9782ac70a195fc2148619d4e68e0
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-22 04:47:33 +0800

    Create coinTradesSlice

--- src/redux/api/coinTrades.ts
+++ src/redux/api/coinTrades.ts
@@ -1,8 +1,24 @@
-import { createEntityAdapter } from '@reduxjs/toolkit'
+import {
+  createEntityAdapter,
+  createSlice,
+  PayloadAction,
+} from '@reduxjs/toolkit'
 
 import { CoinTradeType } from '@customtypes/coins/coin'
+import { RootState } from '@customtypes/store'
 
 const coinTradesAdapter = createEntityAdapter<CoinTradeType>({
   selectId: (coinTrade) => coinTrade?.id,
   sortComparer: (a, b) => a.name.localeCompare(b.name),
+})
+
+const coinTradeSlice = createSlice({
+  name: 'coinTrades',
+  initialState: coinTradesAdapter.getInitialState(),
+  reducers: {
+    upsertCoinTrade: coinTradesAdapter.upsertOne,
+    removeCoinTrade: (state, action: PayloadAction<string>) => {
+      coinTradesAdapter.removeOne(state, action.payload)
+    },
+  },
 })
