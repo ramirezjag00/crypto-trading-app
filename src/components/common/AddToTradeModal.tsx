64d9b892a8de33c07e88bb46ce740fd623f886ee
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-22 03:29:32 +0800

    Port trade card

--- src/components/common/AddToTradeModal.tsx
+++ src/components/common/AddToTradeModal.tsx
@@ -1,184 +1,38 @@
-import React, { useEffect } from 'react'
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-import theme from '@constants/theme'
 import Modal from './Modal'
-import QuantityController from './QuantityController'
 import { CoinAddTradeModalType } from '@customtypes/coins/coin'
-import Button from './Button'
-import { POLLING_INTERVAL } from '@constants/config'
-import { useLazyFetchCoinDetailsQuery } from '@store/api/coinDetails'
-import { addCoinToTradeTitles } from '@constants/coins'
+import TradeCard from './TradeCard'
 
 interface Props {
   isModalVisible: boolean
   onCloseModal: () => void
   activeCoin: CoinAddTradeModalType | null
-  activeCoinQuantity: number
-  onAmountChange: (isIncreasing: boolean) => () => void
 }
 
 const AddToTradeModal: React.FC<Props> = (props) => {
-  const {
-    activeCoin,
-    activeCoinQuantity,
-    isModalVisible,
-    onAmountChange,
-    onCloseModal,
-  } = props
-
-  const [trigger, result] = useLazyFetchCoinDetailsQuery({
-    pollingInterval: POLLING_INTERVAL,
-    refetchOnFocus: true,
-  })
-
-  useEffect(() => {
-    if (activeCoin) {
-      trigger({
-        ids: activeCoin?.id,
-        unit: activeCoin?.unit,
-      })
-    }
-  }, [activeCoin, trigger])
-
-  const currentPrice =
-    result?.data && activeCoin
-      ? result?.data?.[activeCoin?.id]?.[activeCoin?.unit]
-      : 0
+  const { activeCoin, isModalVisible, onCloseModal } = props
 
   const onPressAddToTrades = (): void => {
     onCloseModal()
   }
 
-  const renderItem = ({ item, index }: { item: string; index: number }) => {
-    const value = !index
-      ? currentPrice
-      : index === 1
-      ? activeCoinQuantity
-      : currentPrice * activeCoinQuantity
-    const unit = index === 1 ? activeCoin?.symbol : activeCoin?.unit
-
-    return (
-      <View style={styles.coinPriceContainer}>
-        <Text style={styles.coinMetaLabel}>{item}</Text>
-        <View style={styles.coinQuantity}>
-          <Text style={styles.coinPrice}>{value}</Text>
-          {!!index && (
-            <Text style={styles.coinUnit} numberOfLines={2}>
-              {unit}
-            </Text>
-          )}
-        </View>
-      </View>
-    )
+  const handleModalClose = (): void => {
+    onCloseModal()
   }
 
   return (
     <Modal
       isVisible={isModalVisible}
-      onCloseModal={onCloseModal}
-      hasContainer
-      containerStyle={styles.modalContainer}>
-      <Text style={styles.coinName} numberOfLines={2}>
-        {activeCoin?.name}
-      </Text>
-      <Text style={styles.coinSymbol}>
-        {activeCoin?.symbol} / {activeCoin?.unit}
-      </Text>
-      <FlatList
-        data={addCoinToTradeTitles}
-        renderItem={renderItem}
-        extraData={result?.data}
-        keyExtractor={(item, index) => `${item}-${index}`}
-        scrollEnabled={false}
-      />
-      <QuantityController
-        onPress={onAmountChange}
-        quantity={activeCoinQuantity}
-        isQuantityVisible={false}
-        containerStyle={styles.controllerContainer}
-        controllerStyle={styles.controller}
-      />
-      <Button
-        label="Add to Trades"
-        buttonStyles={styles.tradesContainer}
-        textStyles={styles.tradesLabel}
-        onPress={onPressAddToTrades}
-        disabled={!activeCoinQuantity || !currentPrice}
+      onCloseModal={handleModalClose}
+      hasContainer>
+      <TradeCard
+        buttonLabel="Add to Trades"
+        onPressButton={onPressAddToTrades}
+        activeCoin={activeCoin}
       />
     </Modal>
   )
 }
-
-const styles = StyleSheet.create({
-  modalContainer: {
-    zIndex: 1,
-  },
-  coinName: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: theme?.colors?.white,
-    textDecorationColor: theme?.colors?.primary,
-    textDecorationStyle: 'solid',
-    textDecorationLine: 'underline',
-  },
-  coinSymbol: {
-    fontSize: 12,
-    color: theme?.colors?.white,
-    textTransform: 'uppercase',
-    paddingBottom: 10,
-  },
-  coinPriceContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: theme?.colors?.troutOpacity30,
-    borderRadius: 4,
-    paddingVertical: 10,
-    paddingHorizontal: 10,
-    marginVertical: 5,
-  },
-  coinQuantity: {
-    flexDirection: 'row',
-  },
-  coinMetaLabel: {
-    fontSize: 12,
-    fontWeight: 'bold',
-    color: theme?.colors?.darkShadeLight60,
-  },
-  coinPrice: {
-    fontSize: 12,
-    fontWeight: 'bold',
-    color: theme?.colors?.white,
-  },
-  coinUnit: {
-    fontSize: 12,
-    fontWeight: 'bold',
-    color: theme?.colors?.white,
-    textTransform: 'uppercase',
-    paddingLeft: 10,
-  },
-  controllerContainer: {
-    width: '100%',
-    justifyContent: 'space-between',
-    marginTop: 10,
-  },
-  controller: {
-    width: '48%',
-    borderRadius: 4,
-  },
-  tradesContainer: {
-    backgroundColor: theme?.colors?.primary,
-    borderRadius: 5,
-    padding: 10,
-    marginTop: 10,
-  },
-  tradesLabel: {
-    fontWeight: 'bold',
-    fontSize: 14,
-    textAlign: 'center',
-    color: theme?.colors?.white,
-    textTransform: 'uppercase',
-  },
-})
 
 export default AddToTradeModal
---
+++ src/components/common/TradeCard.tsx
@@ -0,0 +1,223 @@
+import React, { useEffect, useState } from 'react'
+import {
+  Keyboard,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native'
+
+import theme from '@constants/theme'
+import { CoinAddTradeModalType } from '@customtypes/coins/coin'
+import Button from './Button'
+import QuantityController from './QuantityController'
+import { useLazyFetchCoinDetailsQuery } from '@store/api/coinDetails'
+import { POLLING_INTERVAL } from '@constants/config'
+
+interface Props {
+  buttonLabel: string
+  onPressButton: () => void
+  activeCoin: CoinAddTradeModalType | null
+}
+
+const TradeCard: React.FC<Props> = (props) => {
+  const { activeCoin, onPressButton, buttonLabel } = props
+  const [activeCoinQuantity, setActiveCoinQuantity] = useState<number>(0)
+  const [trigger, result] = useLazyFetchCoinDetailsQuery({
+    pollingInterval: POLLING_INTERVAL,
+    refetchOnFocus: true,
+  })
+
+  const currentPrice =
+    result?.data && activeCoin
+      ? result?.data?.[activeCoin?.id]?.[activeCoin?.unit]
+      : 0
+
+  useEffect(() => {
+    if (activeCoin) {
+      trigger({
+        ids: activeCoin?.id,
+        unit: activeCoin?.unit,
+      })
+    }
+  }, [activeCoin, trigger])
+
+  const onAmountQuantityChange =
+    (isIncreasing?: boolean, amount?: number) => (): void => {
+      if (amount) {
+        setActiveCoinQuantity(amount)
+      } else if (!amount && isIncreasing) {
+        setActiveCoinQuantity((value) => value + 1)
+      } else if (!isIncreasing) {
+        setActiveCoinQuantity((value) => (activeCoinQuantity ? value - 1 : 0))
+      }
+    }
+
+  const onChangeAmount = (amount: string): void => {
+    onAmountQuantityChange(undefined, amount ? parseInt(amount, 10) : 0)()
+  }
+
+  return (
+    <View>
+      <Text style={styles.coinName} numberOfLines={2}>
+        {activeCoin?.name}
+      </Text>
+      <Text style={styles.coinSymbol}>
+        {activeCoin?.symbol} / {activeCoin?.unit}
+      </Text>
+      <View style={styles.coinPriceContainer}>
+        <Text style={styles.coinMetaLabel}>Market Price</Text>
+        <View style={styles.coinQuantity}>
+          <Text style={styles.coinPrice}>{currentPrice}</Text>
+        </View>
+      </View>
+      <View style={styles.coinInputContainer}>
+        <View style={styles.coinInput}>
+          <Text style={styles.coinMetaLabel}>Amount</Text>
+          <TextInput
+            style={styles.coinInputBox}
+            onChangeText={onChangeAmount}
+            selectionColor="transparent"
+            underlineColorAndroid="transparent"
+            autoCorrect={false}
+            autoFocus={false}
+            keyboardType={'numeric'}
+            defaultValue={activeCoinQuantity.toString()}
+            onSubmitEditing={Keyboard.dismiss}
+          />
+          <View style={styles.coinQuantity}>
+            <Text style={styles.coinPrice}>{activeCoinQuantity}</Text>
+            <Text style={styles.coinUnit} numberOfLines={2}>
+              {activeCoin?.symbol}
+            </Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.coinPriceContainer}>
+        <Text style={styles.coinMetaLabel}>Total</Text>
+        <View style={styles.coinQuantity}>
+          <Text style={styles.coinPrice}>
+            {currentPrice * activeCoinQuantity}
+          </Text>
+          <Text style={styles.coinUnit} numberOfLines={2}>
+            {activeCoin?.unit}
+          </Text>
+        </View>
+      </View>
+      <QuantityController
+        onPress={onAmountQuantityChange}
+        quantity={activeCoinQuantity}
+        isQuantityVisible={false}
+        containerStyle={styles.controllerContainer}
+        controllerStyle={styles.controller}
+      />
+      <Button
+        label={buttonLabel}
+        buttonStyles={styles.tradesContainer}
+        textStyles={styles.tradesLabel}
+        onPress={onPressButton}
+        disabled={!activeCoinQuantity || !currentPrice}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  coinName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: theme?.colors?.white,
+    textDecorationColor: theme?.colors?.primary,
+    textDecorationStyle: 'solid',
+    textDecorationLine: 'underline',
+  },
+  coinSymbol: {
+    fontSize: 12,
+    color: theme?.colors?.white,
+    textTransform: 'uppercase',
+    paddingBottom: 10,
+  },
+  coinPriceContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: theme?.colors?.troutOpacity30,
+    borderRadius: 4,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    marginVertical: 5,
+  },
+  coinQuantity: {
+    flexDirection: 'row',
+  },
+  coinInputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  coinInput: {
+    position: 'relative',
+    flex: 1,
+    ...Platform.select({
+      ios: {
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+      },
+      android: {
+        paddingVertical: 5,
+        paddingHorizontal: 20,
+        marginTop: 10,
+      },
+    }),
+    borderRadius: 4,
+    backgroundColor: theme?.colors?.troutOpacity30,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  coinInputBox: {
+    flex: 1,
+    marginHorizontal: 8,
+    color: 'transparent',
+    fontSize: 12,
+  },
+  coinMetaLabel: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: theme?.colors?.darkShadeLight60,
+  },
+  coinPrice: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: theme?.colors?.white,
+  },
+  coinUnit: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: theme?.colors?.white,
+    textTransform: 'uppercase',
+    paddingLeft: 10,
+  },
+  controllerContainer: {
+    width: '100%',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+  controller: {
+    width: '48%',
+    borderRadius: 4,
+  },
+  tradesContainer: {
+    backgroundColor: theme?.colors?.primary,
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 10,
+  },
+  tradesLabel: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    textAlign: 'center',
+    color: theme?.colors?.white,
+    textTransform: 'uppercase',
+  },
+})
+
+export default TradeCard
