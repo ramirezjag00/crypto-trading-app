5274571e9421a22eb63619e7d47960db4c254bc2
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-23 02:35:17 +0800

    Add TradesScreen flatlist

--- src/components/screens/Trade/Trades/TradesScreen.tsx
+++ src/components/screens/Trade/Trades/TradesScreen.tsx
@@ -1,14 +1,44 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { FlatList, SafeAreaView, StyleSheet } from 'react-native'
 
 import { useAppSelector } from '@utils/hooks/store'
 import { selectAllCoinTrades } from '@store/api/coinTrades'
+import { CoinTradeType } from '@customtypes/coins/coin'
+import theme from '@constants/theme'
 
 const TradesScreen: React.FC = () => {
   const coinTrades = useAppSelector(selectAllCoinTrades)
-  console.log(coinTrades)
 
-  return <Text>HELLO WORLD</Text>
+  const onPressPurchase = (coinTrade: CoinTradeType): void => {
+    console.log('coinTrade', coinTrade)
+  }
+
+  return (
+    <SafeAreaView style={styles.safeAreaView}>
+      <FlatList
+        contentContainerStyle={styles.contentContainer}
+        data={coinTrades}
+        extraData={coinTrades}
+        keyExtractor={(item) => item?.id}
+        horizontal={false}
+        scrollEnabled
+        scrollEventThrottle={16}
+        removeClippedSubviews={true}
+      />
+    </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    backgroundColor: theme?.colors?.dark,
+    flex: 1,
+  },
+  contentContainer: {
+    alignItems: 'center',
+    alignSelf: 'center',
+    width: '90%',
+  },
+})
 
 export default TradesScreen
