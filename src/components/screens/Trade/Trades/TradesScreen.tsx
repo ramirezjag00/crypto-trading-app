7d22b64a0075f6954f4454122350e81eac5d701a
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-23 02:35:33 +0800

    Add tradesScreen renderItem

--- src/components/screens/Trade/Trades/TradesScreen.tsx
+++ src/components/screens/Trade/Trades/TradesScreen.tsx
@@ -4,6 +4,7 @@ import { FlatList, SafeAreaView, StyleSheet } from 'react-native'
 import { useAppSelector } from '@utils/hooks/store'
 import { selectAllCoinTrades } from '@store/api/coinTrades'
 import { CoinTradeType } from '@customtypes/coins/coin'
+import TradeCard from '@common/TradeCard'
 import theme from '@constants/theme'
 
 const TradesScreen: React.FC = () => {
@@ -13,12 +14,24 @@ const TradesScreen: React.FC = () => {
     console.log('coinTrade', coinTrade)
   }
 
+  const renderItem = ({ item }: { item: CoinTradeType }) => {
+    return (
+      <TradeCard
+        buttonLabel="Purchase"
+        onPressButton={onPressPurchase}
+        activeCoin={item}
+        containerStyle={styles.tradeCard}
+      />
+    )
+  }
+
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <FlatList
         contentContainerStyle={styles.contentContainer}
         data={coinTrades}
         extraData={coinTrades}
+        renderItem={renderItem}
         keyExtractor={(item) => item?.id}
         horizontal={false}
         scrollEnabled
@@ -38,6 +51,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     alignSelf: 'center',
     width: '90%',
+  },
+  tradeCard: {
+    backgroundColor: theme?.colors?.troutOpacity30,
+    borderRadius: 5,
+    marginVertical: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
   },
 })
 
