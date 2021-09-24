b17d05200d4bb3275b6642499105362ffa196aba
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-24 08:09:18 +0800

    Add TradeStack screenOptions for titles and styles

--- src/routes/TradeStack.tsx
+++ src/routes/TradeStack.tsx
@@ -1,18 +1,44 @@
 import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationOptions,
+} from '@react-navigation/native-stack'
 
 import type TradeStackParamList from '@customtypes/navigation/trade'
 import TradesScreen from '@screens/Trade/Trades/TradesScreen'
 import OrdersScreen from '@screens/Trade/Orders/OrdersScreen'
+import theme from '@constants/theme'
 
 const TradeStack = createNativeStackNavigator<TradeStackParamList>()
+
+const handleStackScreenOptions: NativeStackNavigationOptions = {
+  headerStyle: {
+    backgroundColor: theme?.colors?.dark,
+  },
+  headerTintColor: theme?.colors?.white,
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+}
 
 const TradeStackRoutes: React.FC = () => (
   <TradeStack.Navigator
     initialRouteName="TradesScreen"
-    screenOptions={{ headerShown: false }}>
-    <TradeStack.Screen name="TradesScreen" component={TradesScreen} />
-    <TradeStack.Screen name="OrdersScreen" component={OrdersScreen} />
+    screenOptions={handleStackScreenOptions}>
+    <TradeStack.Screen
+      name="TradesScreen"
+      component={TradesScreen}
+      options={{
+        title: 'Trades',
+      }}
+    />
+    <TradeStack.Screen
+      name="OrdersScreen"
+      component={OrdersScreen}
+      options={{
+        title: 'Orders',
+      }}
+    />
   </TradeStack.Navigator>
 )
 
