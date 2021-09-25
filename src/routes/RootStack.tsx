5210ded391cab4c8699ff098e5fab2436300eded
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-26 03:01:26 +0800

    Set up SafeAreaProvicer in RootStack

--- src/routes/RootStack.tsx
+++ src/routes/RootStack.tsx
@@ -4,18 +4,21 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import BottomTabStack from '@routes/BottomTabStack'
 import type RootStackParamList from '@customtypes/navigation/root'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
 
 const RootStack: React.FC = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Main"
-        screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Main" component={BottomTabStack} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Main"
+          screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Main" component={BottomTabStack} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   )
 }
 
