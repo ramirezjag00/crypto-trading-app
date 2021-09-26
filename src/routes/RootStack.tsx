7278a0977deea11c2097492643c8098872f32f48
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-26 09:58:04 +0800

    Add PreAuthStack as initialRouteName in RootStack

--- src/routes/RootStack.tsx
+++ src/routes/RootStack.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import BottomTabStack from '@routes/BottomTabStack'
 import type RootStackParamList from '@customtypes/navigation/root'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
+import PreAuthStack from './PreAuthStack'
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
 
@@ -13,9 +14,10 @@ const RootStack: React.FC = () => {
     <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator
-          initialRouteName="Main"
+          initialRouteName="PreAuth"
           screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Main" component={BottomTabStack} />
+          <Stack.Screen name="PreAuth" component={PreAuthStack} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
