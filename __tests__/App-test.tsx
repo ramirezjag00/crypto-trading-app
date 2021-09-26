1b2c4127bb0110c2b5338adaaa587df911cc8f11
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-26 23:41:22 +0800

    Set up clean up afterEach tests and clear mocks

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,15 +1,13 @@
-/**
- * @format
- */
-
 import 'react-native'
 import React from 'react'
-import App from '../App'
-
-// Note: test renderer must be required after react-native.
+import { cleanup } from '@testing-library/react-native'
 import renderer from 'react-test-renderer'
 
+import App from '../App'
+
 jest.useFakeTimers()
+
+afterEach(cleanup)
 
 it('renders correctly', () => {
   renderer.create(<App />)
