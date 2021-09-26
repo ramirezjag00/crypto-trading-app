aabee33892f2a1dbad922f782b27e62693c0e6b1
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-26 10:03:10 +0800

    Configure dark theme in PinScreen

--- src/components/screens/PreAuth/Pin/PinScreen.tsx
+++ src/components/screens/PreAuth/Pin/PinScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import PINCode from '@haskkor/react-native-pincode'
+import { StyleSheet } from 'react-native'
 import { useNavigation } from '@react-navigation/core'
+import PINCode from '@haskkor/react-native-pincode'
 import RNExitApp from 'react-native-exit-app'
 
 import useUserSetPinCode from '@utils/hooks/useUserSetPinCode'
 import { PreAuthStackNavigationProp } from '@customtypes/navigation/preAuth'
+import theme from '@constants/theme'
 
 const PinScreen: React.FC = () => {
   const withPin = useUserSetPinCode()
@@ -22,8 +24,50 @@ const PinScreen: React.FC = () => {
       finishProcess={onFinishProcess}
       touchIDDisabled
       onClickButtonLockedPage={onQuitApp}
+      delayBetweenAttempts={1500}
+      styleMainContainer={styles.container}
+      colorPassword={theme?.colors?.primary}
+      colorPasswordEmpty={theme?.colors?.primary}
+      colorPasswordError={theme?.colors?.bearishRed}
+      colorCircleButtons={theme?.colors?.troutOpacity30}
+      numbersButtonOverlayColor={theme?.colors?.primary}
+      styleLockScreenButton={styles.lockButton}
+      styleLockScreenMainContainer={styles.container}
+      styleLockScreenText={styles.lockText}
+      styleLockScreenTextTimer={styles.lockText}
+      styleLockScreenViewTimer={styles.lockScreenViewTimer}
+      styleLockScreenTitle={styles.lockScreenTitle}
+      styleLockScreenViewIcon={styles.lockScreenViewIcon}
     />
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.colors?.dark,
+  },
+  lockButton: {
+    backgroundColor: theme.colors?.primary,
+    borderRadius: 4,
+    width: '80%',
+    alignItems: 'center',
+  },
+  lockText: {
+    color: theme?.colors.darkShadeLight20,
+  },
+  lockScreenViewTimer: {
+    borderColor: theme?.colors.darkShadeLight20,
+  },
+  lockScreenTitle: {
+    color: theme?.colors.darkShadeLight20,
+    opacity: 1,
+    fontWeight: '300',
+    fontSize: 25,
+    textAlign: 'center',
+  },
+  lockScreenViewIcon: {
+    display: 'none',
+  },
+})
 
 export default PinScreen
