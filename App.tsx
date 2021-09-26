d08984b2f3cf2d80c363d85a45e7eecfd89dd66a
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-27 01:26:22 +0800

    Add import rn-gesture-handler

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
+import 'react-native-gesture-handler'
 
 import RootStack from '@routes/RootStack'
 import store, { persistor } from '@store/store'
