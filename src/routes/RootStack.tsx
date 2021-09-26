e3e6e31f8f7efab45331ee86bf8f022604332cda
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-27 01:51:37 +0800

    Remove SafeAreaProdiver

--- src/routes/RootStack.tsx
+++ src/routes/RootStack.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import BottomTabStack from '@routes/BottomTabStack'
 import type RootStackParamList from '@customtypes/navigation/root'
@@ -11,16 +10,14 @@ const Stack = createStackNavigator<RootStackParamList>()
 
 const RootStack: React.FC = () => {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="PreAuth"
-          screenOptions={{ headerShown: false, gestureEnabled: false }}>
-          <Stack.Screen name="Main" component={BottomTabStack} />
-          <Stack.Screen name="PreAuth" component={PreAuthStack} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="PreAuth"
+        screenOptions={{ headerShown: false, gestureEnabled: false }}>
+        <Stack.Screen name="Main" component={BottomTabStack} />
+        <Stack.Screen name="PreAuth" component={PreAuthStack} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
