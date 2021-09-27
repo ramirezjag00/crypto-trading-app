1f22b105d26b7e1664e7c74170961d3549b868a0
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-27 18:23:17 +0800

    Create StoreType for mock

---
+++ __tests__/types/storeType.ts
@@ -0,0 +1,8 @@
+import { CoinTradeType, CoinOrderType } from '@customtypes/coins/coin'
+
+type StoreType = {
+  coinTrades: CoinTradeType[]
+  coinOrders: CoinOrderType[]
+}
+
+export default StoreType
