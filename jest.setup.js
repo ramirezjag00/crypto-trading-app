1164dd2cdaaaefc338cef4c126bd4b99e4e9ce36
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-09-27 18:42:02 +0800

    Add createStackNavigator and createBottomTabNavigator mocks

--- jest.setup.js
+++ jest.setup.js
@@ -2,8 +2,12 @@ import 'react-native-gesture-handler/jestSetup'
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock'
 
 jest.mock('react-native-gesture-handler', () => {})
-jest.mock('@react-navigation/stack', () => {})
-jest.mock('@react-navigation/bottom-tabs', () => {})
+jest.mock('@react-navigation/stack', () => ({
+  createStackNavigator: jest.fn()
+}))
+jest.mock('@react-navigation/bottom-tabs', () => ({
+  createBottomTabNavigator: jest.fn()
+}))
 jest.mock('@react-navigation/elements', () => {})
 jest.mock('react-native-safe-area-context', () => {})
 
